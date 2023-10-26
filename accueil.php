5352e79aa527314e4208b782e84fae04796666bf
Nathanaël <88835356+RetroGoy@users.noreply.github.com>
2023-10-26 11:12:09 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -10,11 +10,10 @@ session_start();
 </head>
 <body>
     <?php if (isset($_SESSION['pseudo'])): ?>
-        <!-- Si l'utilisateur est connecté, on affiche son pseudo -->
+        <!-- Afficher le pseudo -->
         <p>Bonjour, <?php echo $_SESSION['pseudo']; ?>!</p>
         <a href="logout.php">Déconnexion</a>
     <?php else: ?>
-        <!-- Si l'utilisateur n'est pas connecté, on affiche un message -->
         <p>Vous n'êtes pas connecté.</p>
     <?php endif; ?>
 </body>
